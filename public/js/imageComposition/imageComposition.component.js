c13d884d0dd27963e4d98f2b8d2acb0168ea9a30
Anna Lotko <annaklotko@gmail.com>
2017-02-23 18:36:48 -0700

    theme and filter widgets are dynamically getting populated from data arrs

--- public/js/imageComposition/imageComposition.component.js
+++ public/js/imageComposition/imageComposition.component.js
@@ -15,14 +15,14 @@
       vm.$onInit = function() {
         vm.composition_settings = JSON.parse(localStorage.getItem('composition_settings'));
         vm.postcardBackground = $sce.trustAsResourceUrl(vm.composition_settings.image_url);
-        vm.curTheme = themes[vm.composition_settings.theme_id];
-        vm.curFilter = filters[vm.composition_settings.color_id];
+        vm.curTheme = vm.themes[vm.composition_settings.theme_id].path;
+        vm.curFilter = vm.filters[vm.composition_settings.color_id].name;
       };
 
       vm.selectColor = function(color_id) {
         console.log('selected color', color_id);
         vm.composition_settings.color_id = color_id;
-        vm.curFilter = filters[color_id];
+        vm.curFilter = vm.filters[color_id].name;
         // Refresh image to reflect current filter
         let curImage = vm.composition_settings.image_url + '?' + new Date().getTime();
         vm.postcardBackground = $sce.trustAsResourceUrl(curImage);
@@ -32,18 +32,38 @@
       vm.selectTheme = function(theme_id) {
         console.log('selected theme', theme_id);
         vm.composition_settings.theme_id = theme_id;
-        vm.curTheme = themes[theme_id];
+        vm.curTheme = vm.themes[theme_id].path;
         // TODO: Center selected item on carousel
-        console.log(vm.composition_settings.greetings_subtext);
       };
 
-      var themes = {
-        0: "M422.67,0H0V306H450V0ZM421,280,225,268.71,29,280,17.3,153,29,26,225,37.29,421,26l11.7,127Z",
-        1: "M422.67,0H0V306H450V0ZM421,280l-196,8.71L29,280l8.3-127L29,26l196-8.71L421,26l-8.3,127Z"
+      vm.nextStep = function() {
+        localStorage.setItem('composition_settings', JSON.stringify(vm.composition_settings));
+        $state.go('messageComposition');
       };
 
-      var filters = {
-        1: "#pictureFilter"
-      }
-    };
+      vm.themes = [
+        {
+          id: 0,
+          asset_name: "pattern.svg",
+          path: "M422.67,0H0V306H450V0ZM421,280,225,268.71,29,280,17.3,153,29,26,225,37.29,421,26l11.7,127Z"
+        }, {
+          id: 1,
+          asset_name: "pattern.svg",
+          path: "M422.67,0H0V306H450V0ZM421,280l-196,8.71L29,280l8.3-127L29,26l196-8.71L421,26l-8.3,127Z"
+        }
+      ];
+
+      vm.filters = [
+        {
+          id: 0,
+          name: "",
+          asset_name: "color.svg"
+        },
+        {
+          id: 1,
+          name: "#pictureFilter",
+          asset_name: "color.svg"
+        }
+      ];
+    }
 }());
