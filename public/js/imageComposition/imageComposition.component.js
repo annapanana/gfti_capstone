76d58f66641394d98a1d10384bc42da5064615b2
Anna Lotko <annaklotko@gmail.com>
2017-02-23 18:12:30 -0700

    updating theme depending on selection is working

--- public/js/imageComposition/imageComposition.component.js
+++ public/js/imageComposition/imageComposition.component.js
@@ -24,7 +24,7 @@
         console.log('selected color', color_id);
         vm.composition_settings.color_id = color_id;
         vm.curFilter = filters[color_id];
-        // Refresh Image
+        // Refresh image to reflect current filter
         let curImage = vm.composition_settings.image_url + '?' + new Date().getTime();
         vm.postcardBackground = $sce.trustAsResourceUrl(curImage);
         // TODO: Center selected item on carousel
@@ -33,11 +33,12 @@
       vm.selectTheme = function(theme_id) {
         console.log('selected theme', theme_id);
         vm.composition_settings.theme_id = theme_id;
-        vm.curFilter = themes[theme_id];
+        vm.curTheme = themes[theme_id];
         // TODO: Center selected item on carousel
       };
 
       var themes = {
+        0: "M422.67,0H0V306H450V0ZM421,280,225,268.71,29,280,17.3,153,29,26,225,37.29,421,26l11.7,127Z",
         1: "M422.67,0H0V306H450V0ZM421,280l-196,8.71L29,280l8.3-127L29,26l196-8.71L421,26l-8.3,127Z"
       };
 
