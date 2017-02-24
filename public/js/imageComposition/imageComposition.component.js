fdb1299f8b8bec1511a6c824e05ee3e3cc213369
Anna Lotko <annaklotko@gmail.com>
2017-02-23 17:34:24 -0700

    added postcard layout to composition

--- public/js/imageComposition/imageComposition.component.js
+++ public/js/imageComposition/imageComposition.component.js
@@ -6,25 +6,30 @@
       controller: controller
     });
 
-    controller.$inject = ["$http", "$state", "$stateParams"];
-    function controller($http, $state, stateParams) {
+    controller.$inject = ["$http", "$state", "$stateParams", "$sce"];
+    function controller($http, $state, stateParams, $sce) {
       const vm = this;
       vm.composition_settings = {};
+
       vm.$onInit = function() {
         vm.composition_settings = JSON.parse(localStorage.getItem('composition_settings'));
         console.log(vm.composition_settings.image_url);
+        vm.postcardBackground = $sce.trustAsResourceUrl(vm.composition_settings.image_url);
+
       };
 
       vm.selectColor = function(color_id) {
         console.log('selected color', color_id);
         vm.composition_settings.color_id = color_id;
         // TODO: Center selected item on carousel
+        // TODO: Update postcard layout
       };
 
       vm.selectTheme = function(theme_id) {
         console.log('selected theme', theme_id);
         vm.composition_settings.theme_id = theme_id;
         // TODO: Center selected item on carousel
+        // TODO: Update postcard layou
       };
     }
 }());
