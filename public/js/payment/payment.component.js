12dea70b48492bf4abcb0d7800658015fe46a4ec
Anna Lotko <annaklotko@gmail.com>
2017-02-25 14:11:20 -0700

    added stripe widget and fixed button layout on home page

--- public/js/payment/payment.component.js
+++ public/js/payment/payment.component.js
@@ -12,16 +12,61 @@
       var postcard = {};
       vm.payment_info = {};
       vm.imagePreview = "http://placehold.it/350x150";
+      var stripe = Stripe('pk_test_1EIBbNvuJSQ8GPIJFBC71eqP');
+      var elements = stripe.elements();
+      // for stripe element
+      var style = {
+        base: {
+          // Add your base input styles here. For example:
+          fontSize: '16px',
+          lineHeight: '24px'
+        }
+      };
+      // Create an instance of the card Element
+      var card = elements.create('card', {style: style});
+      // Add an instance of the card Element into the `card-element` <div>
+      card.mount('#card-element');
 
       vm.$onInit = function() {
         postcard = JSON.parse(localStorage.getItem('postcard'));
         // TODO Load postcard preview settings
         vm.payment_info = postcard.payment_info;
+
+        card.addEventListener('change', function(event) {
+          var displayError = document.getElementById('card-errors');
+          if (event.error) {
+            displayError.textContent = event.error.message;
+          } else {
+            displayError.textContent = '';
+          }
+        });
       };
 
       vm.submitCard = function() {
         postcard.payment_info = vm.payment_info;
-        // Set draw path for html template
+
+        stripe.createToken(card).then(function(result) {
+          if (result.error) {
+            // Inform the user if there was an error
+            var errorElement = document.getElementById('card-errors');
+            errorElement.textContent = result.error.message;
+          } else {
+            // Send the token to your server
+            stripeTokenHandler(result.token);
+          }
+        });
+      };
+
+      const stripeTokenHandler = (token) => {
+        // Insert the token ID into the form so it gets submitted to the server
+        const form = document.getElementById('payment-form');
+        const hiddenInput = document.createElement('input');
+        hiddenInput.setAttribute('type', 'hidden');
+        hiddenInput.setAttribute('name', 'stripeToken');
+        hiddenInput.setAttribute('value', token.id);
+        form.appendChild(hiddenInput);
+
+        // Submit the form
         $http.post('/postcards', postcard).then((result) => {
           console.log("result", result.data);
 
