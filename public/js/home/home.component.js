fdc500add3c10840a616f9a1905f64eea3f31b07
Anna Lotko <annaklotko@gmail.com>
2017-03-01 14:15:53 -0700

    fixed postcard previews

--- public/js/home/home.component.js
+++ public/js/home/home.component.js
@@ -56,6 +56,10 @@
         $(".card-preview").flip();
       };
 
+      vm.openh1 = false;
+      vm.openh2 = false;
+      vm.openh3 = false;
+      
       vm.hoverObject = "";
       vm.setHoverObject = function(obj) {
         vm.hoverObject = obj;
