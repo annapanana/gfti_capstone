9d8ef0fa48bfb80096136b7d8ad49e689e545503
Anna Lotko <annaklotko@gmail.com>
2017-03-06 14:27:15 -0700

    fixed photo url submit functionality

--- public/js/app/postcard.service.js
+++ public/js/app/postcard.service.js
@@ -10,6 +10,7 @@
         let color_id = this.postcard.composition_settings.color_id;
         let theme_id = this.postcard.composition_settings.theme_id;
         this.postcard.color_hex = colorData[theme_id][color_id];
+        localStorage.setItem('postcard', JSON.stringify(this.postcard));
       };
 
       this.getCompositionSettings = function() {
